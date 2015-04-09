13f47fd086731fa92b290d3f0b219826dce71529
Jason Dunkelberger (dirkraft) <dirkraft@gmail.com>
2015-04-09 18:02:24 +0000

    bind sliders to together to reasonable values

--- ui/app.controller.js
+++ ui/app.controller.js
@@ -9,6 +9,8 @@
         board = Vis.CFG.board,
         Util = Vis.Util;
 
+    vm.minDataPoints = 10;
+    vm.maxDataPoints = 1000;
 
     vm.selectedWindowIdx = 0;
     vm.windowOpts = Util.toDurations(['2 weeks', '10 days', '1 week', '6 days', '5 days', '4 days', '3 days',
@@ -23,24 +25,26 @@
       // produce a tiny number of data points. Drag it along to a reasonable value if necessary.
       var window = vm.windowOpts[vm.selectedWindowIdx];
       var period = vm.periodOpts[vm.selectedPeriodIdx];
-      var granules = window / period;
-      if (granules < 0) {
-        // move up to a period smaller than the window
-      }
-      if (granules < 10) {
+      while (window / period < vm.minDataPoints && vm.selectedPeriodIdx < vm.periodOpts.length - 1) {
         // move up to a period that fits in the window at least 10 times
+        period = vm.periodOpts[++vm.selectedPeriodIdx];
+      }
+      while (window / period > vm.maxDataPoints && vm.selectedPeriodIdx > 0) {
+        // or, move down to a period that produces less than 1000 data points
+        period = vm.periodOpts[--vm.selectedPeriodIdx];
       }
     };
     vm.periodUpdated = function () {
       // Corollary to .windowUpdated
       var window = vm.windowOpts[vm.selectedWindowIdx];
       var period = vm.periodOpts[vm.selectedPeriodIdx];
-      var granules = window / period;
-      if (granules < 0) {
-        // move up to a window larger than the period
-      }
-      if (granules < 10) {
+      while (window / period < vm.minDataPoints && vm.selectedWindowIdx > 0) {
         // move up to a window that fits the period at least 10 times
+        window = vm.windowOpts[--vm.selectedWindowIdx];
+      }
+      while (window / period > vm.maxDataPoints && vm.selectedWindowIdx < vm.windowOpts.length - 1) {
+        // or, move down to a window that produces less than 1000 data points
+        window = vm.windowOpts[++vm.selectedWindowIdx];
       }
     };
 
@@ -53,6 +57,7 @@
     for (var i = vm.windowOpts.length - 1; vm.windowOpts[vm.selectedWindowIdx] <= largestWindow && i >= 0; i--) {
       vm.selectedWindowIdx = i;
     }
+    vm.windowUpdated();
 
 
     $q.all(board.things
@@ -62,7 +67,7 @@
         .then(function (metrics) {
           var flotData = Charting.toFlotData(board, metrics);
 
-          $('#chart-ctn')
+          var flot = $('#chart-ctn')
               .plot(flotData, {
                 legend: {
                   show: true
@@ -74,6 +79,14 @@
                 }
               })
               .data('plot');
+
+          // Stay with window size.
+          $(window).resize(function () {
+            flot.resize();
+            flot.setupGrid();
+            flot.draw();
+          });
+
         }, Util.defaultErrorHandler);
 
   }
